18a6b2b65e878bf4fa131499dc9e45dc1c9573d1
anusreemj <anusree.nair@gmail.com>
2026-06-10 15:48:34 +0530

    Initial commit

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "My App",
+  description: "My Next.js App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+      <html lang="en">
+      <body>{children}</body>
+      </html>
+  );
+}
